a2ff4f28e710d2b1954bcb02811d5116756a57de
Jack Hanford <jackhanford@gmail.com>
2018-09-25 09:48:36 -0700

    remove config.wobbly from staggered

--- packages/staggered/src/staggered-down.js
+++ packages/staggered/src/staggered-down.js
@@ -1,33 +1,33 @@
 import * as React from "react";
-import { Trail, config } from "react-spring";
+import { Trail } from "react-spring";
 import idx from "idx";
 
 export default class StaggeredReveal extends React.Component {
   static defaultProps = {
-    reveal: true
+    reveal: true,
+    from: -40
   };
 
   render() {
-    const { reveal, children, ...props } = this.props;
+    const { reveal, children, from, ...props } = this.props;
 
     return (
       <Trail
         {...props}
         from={{
           opacity: 0,
-          translateY: -40
+          translateY: from
         }}
         to={{
           opacity: reveal ? 1 : 0,
-          translateY: reveal ? 0 : -40
+          translateY: reveal ? 0 : from
         }}
         keys={React.Children.map(children, (child, index) => index)}
-        config={config.wobbly}
       >
         {React.Children.map(
           children,
           child => ({ opacity, scale, translateY }) => {
-            if (opacity === 0 && translateY === -40) return null;
+            if (opacity === 0 && translateY === from) return null;
 
             const childStyle = idx(child, _ => _.child.props.style) || {};
             const style = {
--- packages/staggered/src/staggered-left.js
+++ packages/staggered/src/staggered-left.js
@@ -1,33 +1,33 @@
 import * as React from "react";
-import { Trail, config } from "react-spring";
+import { Trail } from "react-spring";
 import idx from "idx";
 
 export default class StaggeredReveal extends React.Component {
   static defaultProps = {
-    reveal: true
+    reveal: true,
+    from: -40
   };
 
   render() {
-    const { reveal, children, ...props } = this.props;
+    const { reveal, children, from, ...props } = this.props;
 
     return (
       <Trail
         {...props}
         from={{
           opacity: 0,
-          translateX: -40
+          translateX: from
         }}
         to={{
           opacity: reveal ? 1 : 0,
-          translateX: reveal ? 0 : -40
+          translateX: reveal ? 0 : from
         }}
         keys={React.Children.map(children, (child, index) => index)}
-        config={config.wobbly}
       >
         {React.Children.map(
           children,
           child => ({ opacity, scale, translateX }) => {
-            if (opacity === 0 && translateX === -40) return null;
+            if (opacity === 0 && translateX === from) return null;
 
             const childStyle = idx(child, _ => _.child.props.style) || {};
             const style = {
